0e771523a842d8f401dc5ae920f595833279dd86
Timothy <tjyu1040@gmail.com>
2015-04-09 23:56:13 -0400

    Updated MainFragment.

--- app/src/main/java/com/ephemeraldreams/gallyshuttle/ui/MainFragment.java
+++ app/src/main/java/com/ephemeraldreams/gallyshuttle/ui/MainFragment.java
@@ -17,22 +17,70 @@
 package com.ephemeraldreams.gallyshuttle.ui;
 
 import android.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ephemeraldreams.gallyshuttle.R;
+import com.ephemeraldreams.gallyshuttle.api.ShuttleApiService;
+import com.ephemeraldreams.gallyshuttle.api.models.ApiResponse;
+import com.ephemeraldreams.gallyshuttle.data.CacheManager;
+import com.ephemeraldreams.gallyshuttle.data.models.Schedule;
+import com.ephemeraldreams.gallyshuttle.util.DateUtils;
+
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.Period;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import butterknife.InjectView;
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 /**
  * Fragment to show schedule options for users to select.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements Observer<ApiResponse>, AdapterView.OnItemSelectedListener {
+
+    private static final int SATURDAY = 6;
+    private static final int SUNDAY = 7;
+
+    @InjectView(R.id.current_schedule_text_view) TextView currentScheduleTextView;
+    @InjectView(R.id.station_spinner) Spinner spinner;
+    @InjectView(R.id.time_countdown_text_view) TextView timeCountDownTextView;
+
+    @Inject ShuttleApiService shuttleApiService;
+    @Inject CacheManager cacheManager;
+    private BroadcastReceiver networkStateBroadCastReceiver;
+    private boolean isNetworkStateBroadcastReceiverRegistered;
+
+    private int scheduleId;
+    private Schedule schedule;
+    @Inject Resources resources;
+
+    private LocalDateTime arrivalTime;
 
     /**
      * Required empty public constructor
@@ -45,11 +93,59 @@ public class MainFragment extends Fragment {
         return new MainFragment();
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((BaseActivity) getActivity()).inject(this);
+
+        LocalDateTime now = LocalDateTime.now();
+        int hour = now.getHourOfDay();
+        if (hour >= 10) {
+            scheduleId = R.array.late_night_stations;
+        } else {
+            int day = now.getDayOfWeek();
+            switch (day) {
+                case SATURDAY:
+                case SUNDAY:
+                    scheduleId = R.array.weekend_stations;
+                    break;
+                default:
+                    scheduleId = R.array.continuous_stations;
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (networkStateBroadCastReceiver != null && !isNetworkStateBroadcastReceiverRegistered) {
+            registerNetworkBroadcastReceiver();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (networkStateBroadCastReceiver != null && isNetworkStateBroadcastReceiverRegistered) {
+            unregisterNetworkBroadcastReceiver();
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.inject(this, rootView);
+
+        currentScheduleTextView.setText(Schedule.getScheduleTitle(scheduleId, getResources()));
+
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getActivity(), scheduleId, R.layout.support_simple_spinner_dropdown_item);
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(this);
+
+        loadSchedule();
         return rootView;
     }
 
@@ -59,25 +155,145 @@ public class MainFragment extends Fragment {
         ButterKnife.reset(this);
     }
 
-    @OnClick({R.id.continuous_button, R.id.alt_continuous_button, R.id.late_night_button,
-            R.id.weekend_button, R.id.modified_button})
-    public void buildScheduleFragment(Button button) {
-        switch (button.getId()) {
-            case R.id.continuous_button:
-                ScheduleActivity.launchActivity(getActivity(), ScheduleActivity.CONTINUOUS);
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        loadSchedule();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        //TODO: find next time for specified station.
+        //startCountDown(0);
+    }
+
+    private void calculateTimeToArrival(int position) {
+        List<LocalTime> stationTimes = schedule.getTimes(position);
+        LocalDateTime now = LocalDateTime.now();
+
+        for (LocalTime time : stationTimes) {
+            if (time.toDateTimeToday().toLocalDateTime().isAfter(now)) {
+                arrivalTime = time.toDateTimeToday().toLocalDateTime();
+            }
+        }
+
+        if (arrivalTime == null) {
+            arrivalTime = new LocalDateTime();
+        }
+
+        long millisInFuture = Period.fieldDifference(now, arrivalTime).getMillis();
+
+        startCountDown(millisInFuture);
+    }
+
+    private void startCountDown(long millisInFuture) {
+        new CountDownTimer(millisInFuture, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeCountDownTextView.setText("Minutes remaining: " + DateUtils.convertMillisecondsToMinutes(millisUntilFinished));
+            }
+
+            @Override
+            public void onFinish() {
+                //calculateTimeToArrival(storedPosition);
+                timeCountDownTextView.setText("Done!");
+            }
+        }.start();
+    }
+
+    /**
+     * Attempt to load from cache or download schedule from web.
+     */
+    private void loadSchedule() {
+
+        String title = Schedule.getScheduleTitle(scheduleId, resources);
+
+        if (cacheManager.scheduleCacheFileExists(cacheManager.getScheduleFile(title))) {
+            try {
+                schedule = cacheManager.readScheduleCacheFile(title);
+                onCompleted();
+            } catch (FileNotFoundException e) {
+                Timber.e(e, "Cached file for " + title + " schedule not found.");
+                loadScheduleFromWeb();
+            }
+        } else {
+            loadScheduleFromWeb();
+        }
+    }
+
+    /**
+     * Download schedule data from web.
+     */
+    private void loadScheduleFromWeb() {
+        Observable<ApiResponse> apiResponseObservable;
+        switch (scheduleId) {
+            case R.array.continuous_stations:
+                apiResponseObservable = shuttleApiService.getContinuousSchedule();
                 break;
-            case R.id.alt_continuous_button:
-                ScheduleActivity.launchActivity(getActivity(), ScheduleActivity.ALT_CONTINUOUS);
+            case R.array.alt_continuous_stations:
+                apiResponseObservable = shuttleApiService.getAlternativeContinuousSchedule();
                 break;
-            case R.id.late_night_button:
-                ScheduleActivity.launchActivity(getActivity(), ScheduleActivity.LATE_NIGHT);
+            case R.array.late_night_stations:
+                apiResponseObservable = shuttleApiService.getLateNightSchedule();
                 break;
-            case R.id.weekend_button:
-                ScheduleActivity.launchActivity(getActivity(), ScheduleActivity.WEEKEND);
+            case R.array.weekend_stations:
+                apiResponseObservable = shuttleApiService.getWeekendSchedule();
                 break;
-            case R.id.modified_button:
-                ScheduleActivity.launchActivity(getActivity(), ScheduleActivity.MODIFIED);
+            case R.array.modified_stations:
+                apiResponseObservable = shuttleApiService.getModifiedSchedule();
+                break;
+            default:
+                apiResponseObservable = shuttleApiService.getContinuousSchedule();
                 break;
         }
+        apiResponseObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this);
+    }
+
+    @Override
+    public void onNext(ApiResponse apiResponse) {
+        schedule = new Schedule(scheduleId, apiResponse, resources);
+        cacheManager.createScheduleCacheFile(schedule);
+    }
+
+    @Override
+    public void onCompleted() {
+        //TODO: startCountDown()
+        calculateTimeToArrival(0);
+        Timber.d("Download complete.");
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        Timber.e(e, "Error downloading schedule.");
+        Toast.makeText(getActivity(), "Error loading schedule. Please check your Internet connection.", Toast.LENGTH_LONG).show();
+        registerNetworkBroadcastReceiver();
+    }
+
+    /**
+     * Register a {@link #networkStateBroadCastReceiver}, which monitor for Internet connection
+     * re-establishment.
+     */
+    private void registerNetworkBroadcastReceiver() {
+        networkStateBroadCastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // Internet connection has been re-established.
+                unregisterNetworkBroadcastReceiver();
+            }
+        };
+        getActivity().registerReceiver(networkStateBroadCastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        isNetworkStateBroadcastReceiverRegistered = true;
+    }
+
+    /**
+     * Unregister {@link #networkStateBroadCastReceiver} and try loading schedule from web.
+     */
+    private void unregisterNetworkBroadcastReceiver() {
+        loadScheduleFromWeb();
+        getActivity().unregisterReceiver(networkStateBroadCastReceiver);
+        isNetworkStateBroadcastReceiverRegistered = false;
+        networkStateBroadCastReceiver = null;
+        Timber.d("Network broadcast receiver dismissed.");
     }
 }
