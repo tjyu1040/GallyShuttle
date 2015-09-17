a5c66664eb36e5b7d52d5d1198484f32cc5aa941
Timothy Yu <tjyu1040@gmail.com>
2015-09-17 16:04:22 -0700

    Added ApplicationScope and updated application component and module.

--- mobile/src/main/java/com/ephemeraldreams/gallyshuttle/ApplicationComponent.java
+++ mobile/src/main/java/com/ephemeraldreams/gallyshuttle/ApplicationComponent.java
@@ -1,16 +1,55 @@
 package com.ephemeraldreams.gallyshuttle;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.NotificationManager;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
 
-import javax.inject.Singleton;
+import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.AlarmReminderLength;
+import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.AlarmRingtoneChoice;
+import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.AlarmVibration;
+import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.NotificationRingtoneChoice;
+import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.NotificationVibration;
+import com.ephemeraldreams.gallyshuttle.annotations.scopes.ApplicationScope;
+import com.ephemeraldreams.gallyshuttle.content.CacheManager;
+import com.ephemeraldreams.gallyshuttle.content.ContentModule;
+import com.ephemeraldreams.gallyshuttle.content.preferences.BooleanPreference;
+import com.ephemeraldreams.gallyshuttle.content.preferences.StringPreference;
+import com.ephemeraldreams.gallyshuttle.net.gcm.RegistrationIntentService;
+import com.ephemeraldreams.gallyshuttle.ui.receivers.NetworkStateBroadCastReceiver;
+import com.google.gson.Gson;
+import com.squareup.otto.Bus;
 
 import dagger.Component;
 
-@Singleton
-@Component(modules = ApplicationModule.class)
+/**
+ * A component whose lifetime is constrained to {@link Application}'s lifecycle.
+ */
+@ApplicationScope
+@Component(modules = {ApplicationModule.class, ContentModule.class})
 public interface ApplicationComponent {
 
     void inject(GallyShuttleApplication gallyShuttleApplication);
+    void inject(RegistrationIntentService registrationIntentService);
+    void inject(NetworkStateBroadCastReceiver networkStateBroadCastReceiver);
 
+    // Android system services dependencies made available to sub-graphs
     Application application();
+    Bus bus();
+    ConnectivityManager connectivityManager();
+    AlarmManager alarmManager();
+    NotificationManager notificationManager();
+
+    // Content dependencies made available to sub-graphs
+    Resources resources();
+    SharedPreferences sharedPreferences();
+    @AlarmReminderLength StringPreference alarmReminderLengthStringPreference();
+    @AlarmRingtoneChoice StringPreference alarmRingtoneChoiceStringPreference();
+    @AlarmVibration BooleanPreference alarmVibrationBooleanPreference();
+    @NotificationRingtoneChoice StringPreference notificationRingtoneChoiceStringPreference();
+    @NotificationVibration BooleanPreference notificationVibrationBooleanPreference();
+    Gson gson();
+    CacheManager cacheManager();
 }
--- mobile/src/main/java/com/ephemeraldreams/gallyshuttle/ApplicationModule.java
+++ mobile/src/main/java/com/ephemeraldreams/gallyshuttle/ApplicationModule.java
@@ -1,24 +1,57 @@
 package com.ephemeraldreams.gallyshuttle;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
 
-import javax.inject.Singleton;
+import com.ephemeraldreams.gallyshuttle.annotations.scopes.ApplicationScope;
+import com.squareup.otto.Bus;
 
 import dagger.Module;
 import dagger.Provides;
 
-@Module
+/**
+ * A module for Android-specific system services dependencies which require an {@link Application}
+ * to create.
+ */
+@Module()
 public class ApplicationModule {
 
     private final Application application;
 
-    public ApplicationModule(Application application){
+    public ApplicationModule(Application application) {
         this.application = application;
     }
 
     @Provides
-    @Singleton
-    Application provideApplicationContext(){
+    @ApplicationScope
+    Application application() {
         return application;
+    }
+
+    @Provides
+    @ApplicationScope
+    Bus bus() {
+        return new Bus();
+    }
+
+    @Provides
+    @ApplicationScope
+    ConnectivityManager connectivityManager(Application application) {
+        return (ConnectivityManager) application.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    @Provides
+    @ApplicationScope
+    AlarmManager alarmManager(Application application) {
+        return (AlarmManager) application.getSystemService(Context.ALARM_SERVICE);
+    }
+
+    @Provides
+    @ApplicationScope
+    NotificationManager notificationManager(Application application) {
+        return (NotificationManager) application.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 }
