d653f817723bd2457da0a71b0a57fabe914a09b7
Timothy Yu <tjyu1040@gmail.com>
2015-09-20 16:09:16 -0700

    Added CrashReportingTree with Crashlytics logging.

---
+++ mobile/src/main/java/com/ephemeraldreams/gallyshuttle/util/CrashReportingTree.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 Timothy Yu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ephemeraldreams.gallyshuttle.util;
+
+import com.crashlytics.android.Crashlytics;
+
+import timber.log.Timber;
+
+/**
+ * A tree which logs important information for crash reporting using Crashlytics.
+ */
+public class CrashReportingTree extends Timber.Tree {
+
+    @Override
+    protected void log(int priority, String tag, String message, Throwable throwable) {
+        Crashlytics.log(priority, tag, message);
+        if (throwable != null) {
+            Crashlytics.logException(throwable);
+        }
+    }
+}
