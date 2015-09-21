6ccd3f175d82842c76331275c9e9a2f98f2dd55c
Timothy Yu <tjyu1040@gmail.com>
2015-09-20 17:06:40 -0700

    Added Gson list deserializers for schedules and stations.

---
+++ mobile/src/main/java/com/ephemeraldreams/gallyshuttle/net/api/ListSchedulesDeserializer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 Timothy Yu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ephemeraldreams.gallyshuttle.net.api;
+
+import com.ephemeraldreams.gallyshuttle.net.api.models.Schedule;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+/**
+ * List deserializer for schedules.
+ */
+public class ListSchedulesDeserializer implements JsonDeserializer<List<Schedule>> {
+
+    private final Type listType = new TypeToken<List<Schedule>>() {}.getType();
+
+    @Override
+    public List<Schedule> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        JsonArray jsonArray = jsonObject.get("items").getAsJsonArray();
+        return new Gson().fromJson(jsonArray, listType);
+    }
+}
