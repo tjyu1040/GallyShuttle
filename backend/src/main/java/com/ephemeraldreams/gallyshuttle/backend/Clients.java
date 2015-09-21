7ab87919d926432f948bfa7025a4c76bf20d9597
Timothy Yu <tjyu1040@gmail.com>
2015-09-20 17:24:37 -0700

    Added client whitelist.

---
+++ backend/src/main/java/com/ephemeraldreams/gallyshuttle/backend/Clients.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014 Timothy Yu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ephemeraldreams.gallyshuttle.backend;
+
+/**
+ * TODO: replace client ids.
+ *
+ * Client ids constants.
+ */
+public class Clients {
+
+    public static final String WEB_CLIENT_ID = "replace this with your web client ID";
+    public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+    public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+    public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+    public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
