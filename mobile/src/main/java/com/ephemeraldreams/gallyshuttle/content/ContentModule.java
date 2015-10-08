a9c29267207010fba8b4ecd3f1c97cbcafc42eb9
Timothy Yu <tjyu1040@gmail.com>
2015-10-07 22:28:24 -0700

    Removed Preference objects and annotations in favor of using SharedPreferences directly.

--- mobile/src/main/java/com/ephemeraldreams/gallyshuttle/content/ContentModule.java
+++ mobile/src/main/java/com/ephemeraldreams/gallyshuttle/content/ContentModule.java
@@ -20,18 +20,9 @@ import android.app.Application;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
 
 import com.ephemeraldreams.gallyshuttle.ApplicationModule;
-import com.ephemeraldreams.gallyshuttle.R;
-import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.AlarmReminderLength;
-import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.AlarmRingtoneChoice;
-import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.AlarmVibration;
-import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.NotificationRingtoneChoice;
-import com.ephemeraldreams.gallyshuttle.annotations.qualifiers.NotificationVibration;
 import com.ephemeraldreams.gallyshuttle.annotations.scopes.ApplicationScope;
-import com.ephemeraldreams.gallyshuttle.content.preferences.BooleanPreference;
-import com.ephemeraldreams.gallyshuttle.content.preferences.StringPreference;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -47,10 +38,6 @@ import timber.log.Timber;
 @Module(includes = ApplicationModule.class)
 public class ContentModule {
 
-    public static final String DEFAULT_ALARM_RINGTONE = Settings.System.DEFAULT_ALARM_ALERT_URI.toString();
-    public static final String DEFAULT_NOTIFICATION_RINGTONE = Settings.System.DEFAULT_NOTIFICATION_URI.toString();
-    public static final boolean DEFAULT_VIBRATION_ENABLED = true;
-
     @Provides
     @ApplicationScope
     Resources resources(Application application) {
@@ -61,56 +48,6 @@ public class ContentModule {
     @ApplicationScope
     SharedPreferences sharedPreferences(Application application) {
         return PreferenceManager.getDefaultSharedPreferences(application);
-    }
-
-    @Provides
-    @ApplicationScope
-    @AlarmReminderLength
-    StringPreference alarmReminderLengthPreference(SharedPreferences sharedPreferences, Resources resources) {
-        return new StringPreference(sharedPreferences,
-                resources.getString(R.string.key_alarm_reminder_length_preference),
-                "5"
-        );
-    }
-
-    @Provides
-    @ApplicationScope
-    @AlarmRingtoneChoice
-    StringPreference alarmRingtoneChoicePreference(SharedPreferences sharedPreferences, Resources resources) {
-        return new StringPreference(sharedPreferences,
-                resources.getString(R.string.key_alarm_ringtone_preference),
-                DEFAULT_ALARM_RINGTONE
-        );
-    }
-
-    @Provides
-    @ApplicationScope
-    @AlarmVibration
-    BooleanPreference alarmVibrationPreference(SharedPreferences sharedPreferences, Resources resources) {
-        return new BooleanPreference(sharedPreferences,
-                resources.getString(R.string.key_alarm_vibrate_preference),
-                DEFAULT_VIBRATION_ENABLED
-        );
-    }
-
-    @Provides
-    @ApplicationScope
-    @NotificationRingtoneChoice
-    StringPreference notificationRingtoneChoicePreference(SharedPreferences sharedPreferences, Resources resources) {
-        return new StringPreference(sharedPreferences,
-                resources.getString(R.string.key_notification_ringtone_preference),
-                DEFAULT_NOTIFICATION_RINGTONE
-        );
-    }
-
-    @Provides
-    @ApplicationScope
-    @NotificationVibration
-    BooleanPreference notificationVibrationPreference(SharedPreferences sharedPreferences, Resources resources) {
-        return new BooleanPreference(sharedPreferences,
-                resources.getString(R.string.key_notification_vibrate_preference),
-                DEFAULT_VIBRATION_ENABLED
-        );
     }
 
     @Provides
