205c73419f527d305f54fbba51cdc97369005906
Timothy Yu <tjyu1040@gmail.com>
2015-10-07 22:29:00 -0700

    Added day constants.

--- mobile/src/main/java/com/ephemeraldreams/gallyshuttle/content/DateUtils.java
+++ mobile/src/main/java/com/ephemeraldreams/gallyshuttle/content/DateUtils.java
@@ -39,6 +39,15 @@ public final class DateUtils {
      */
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormat.forPattern("hh:mm aa");
 
+    // Day constants.
+    public static final int MONDAY = 1;
+    public static final int TUESDAY = 2;
+    public static final int WEDNESDAY = 3;
+    public static final int THURSDAY = 4;
+    public static final int FRIDAY = 5;
+    public static final int SATURDAY = 6;
+    public static final int SUNDAY = 7;
+
     private DateUtils() {
         // No instance.
     }
