54251a5a05e1a722cc50fb3c49bb18b199a272dc
Timothy Yu <tjyu1040@gmail.com>
2015-10-25 15:08:31 -0700

    Optimized imports for WebViewActivity.

--- mobile/src/main/java/com/ephemeraldreams/gallyshuttle/ui/WebViewActivity.java
+++ mobile/src/main/java/com/ephemeraldreams/gallyshuttle/ui/WebViewActivity.java
@@ -22,7 +22,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
